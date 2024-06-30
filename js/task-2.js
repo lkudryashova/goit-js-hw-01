1b35707904060936250e81ae3c36727dae339a29
Lesya Kudryashova <lkudryashova.work@gmail.com>
2024-06-30 20:29:47 +0200

    HW 01

---
+++ js/task-2.js
@@ -0,0 +1,9 @@
+function getShippingMessage(country, price, deliveryFee) {
+  console.log(
+    `Shipping to ${country} will cost ${price + deliveryFee} credits!`
+  );
+}
+
+getShippingMessage("Australia", 120, 50);
+getShippingMessage("Germany", 80, 20);
+getShippingMessage("Sweden", 100, 20);
